f26b6dc4a1c40526b5b028e243fc9c12624b66ab
Dhanush18100 <dhanush.nayak.100@gmail.com>
2026-05-15 15:10:14 +0530

    db connection

--- server/config/db.js
+++ server/config/db.js
@@ -1,13 +1,18 @@
 const mongoose = require('mongoose');
 
+let isConnected = false;
+
 const connectDB = async () => {
+    if (isConnected) return;
+
     try {
         await mongoose.connect(process.env.MONGODB_URI);
+
+        isConnected = true;
 
         console.log("MongoDB Connected");
     } catch (error) {
         console.log(error);
-        process.exit(1);
     }
 };
 
